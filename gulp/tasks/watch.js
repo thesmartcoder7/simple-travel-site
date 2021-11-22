cc4a39d51d9f178bc8e9b85a14ed5cff002b7c2f
Samuel Martins <samuel.martins4.sm@gmail.com>
2021-11-22 05:12:37 +0300

    styling testimonial

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,22 +1,22 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
 
 gulp.task("watch", function (done) {
-
     browserSync.init({
         notify: false,
         server: {
-            baseDir: "app"
-        }
-    })
+            baseDir: "app",
+        },
+    });
 
-    gulp.watch('./app/index.html').on('change', browserSync.reload);
+    gulp.watch("./app/index.html").on("change", browserSync.reload);
 
-    gulp.watch('./app/assets/styles/**/*.css', gulp.registry().get('cssInject')) 
-
+    gulp.watch(
+        "./app/assets/styles/**/*.css",
+        gulp.registry().get("cssInject")
+    );
 });
 
-gulp.task("cssInject", gulp.series('styles'), function(){
-    return gulp.src('./app/temp/styles/styles.css')
-    .pipe(browserSync.stream())
-})
+gulp.task("cssInject", gulp.series("styles"), function () {
+    return gulp.src("./app/temp/styles/styles.css").pipe(browserSync.stream());
+});
